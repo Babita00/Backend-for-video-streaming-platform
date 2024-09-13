453248dcecce924f8632592640c74975b0ba9d54
Babita00 <babitaasth07@gmail.com>
2024-09-13 21:06:22 +0545

    multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+
+//disk storage //get from https://github.com/expressjs/multer
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB
+});
+export { upload };
