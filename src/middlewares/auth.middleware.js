6b13bdad3fbe0fda4a8ab7f4aa271c6023a8f664
Babita00 <babitaasth07@gmail.com>
2024-09-15 09:38:34 +0545

    middleware for verifying access token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,12 +1,26 @@
 import { ApiError } from "../utils/APIerror";
 import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 export const verifyJwt = asyncHandler(async (req, res, next) => {
-  const Token =
-    req.cookies?.accessToken ||
-    req.header("Authorization")?.replace("Bearer", "");
-  if (!Token) {
-    throw new ApiError(401, "Unauthorized request");
+  try {
+    const Token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer", "");
+    if (!Token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const decodedToken = jwt.verify(Token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refrehToken "
+    );
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
   }
-  jwt.verify(Token, process.env.accessToken);
 });
