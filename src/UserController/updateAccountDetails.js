74547e5102258ebde17fa6f837c81d03a9311442
Babita00 <babitaasth07@gmail.com>
2024-09-17 21:11:27 +0545

    update account details

---
+++ src/UserController/updateAccountDetails.js
@@ -0,0 +1,27 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/apiResponse";
+
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullname, username, email } = req.body;
+
+  if (!fullname || !email || !username) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const user = User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullname: fullname,
+        email: email,
+        username: username,
+      },
+    },
+    { new: true }
+  ).select("-password ");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Account details updated successfully"));
+});
+
+export { updateAccountDetails };
